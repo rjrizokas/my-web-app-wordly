7c9f47e759b1d51c525a524bd25673b4e3398b7d
rjrizokas <152369340+rjrizokas@users.noreply.github.com>
2024-07-25 20:20:01 +0200

    Update app.py

--- app.py
+++ app.py
@@ -7,21 +7,21 @@ CORS(app)
 
 # Переменные для хранения слов
 words = {
-    "monday": "СЛОВО",
-    "tuesday": "КНИГА",
-    "wednesday": "СРЕДА",
-    "thursday": "БУКВА",
-    "friday": "КРЕСТ",
-    "saturday": "ВЕСНА",
-    "sunday": "ЗВЕНО"
+    "monday": "WORLD",
+    "tuesday": "PLANE",
+    "wednesday": "STAKE",
+    "thursday": "BROWN",
+    "friday": "SWEET",
+    "saturday": "BLIND",
+    "sunday": "STONE"
 }
 
 @app.route('/get_word', methods=['GET'])
 def get_word():
-    # Вернем слово для текущего дня недели
-    day_of_week = datetime.datetime.now().strftime('%A').lower()
+    # Если параметр day_of_week отсутствует, используем текущий день недели
+    day_of_week = request.args.get('day_of_week', datetime.datetime.now().strftime('%A').lower())
     word = words.get(day_of_week, "SNAKE")
-    return jsonify({"word": word}, ensure_ascii=False)  # Обновлено для возврата кириллицы
+    return jsonify({"word": word})  # Возвращаем ответ с корректным кодированием
 
 @app.route('/update_word', methods=['POST'])
 def update_word():
@@ -29,7 +29,7 @@ def update_word():
     for key in data:
         if key in words:
             words[key] = data[key]
-    return jsonify({"message": "Words updated successfully!"}, ensure_ascii=False)  # Обновлено для возврата кириллицы
+    return jsonify({"message": "Words updated successfully!"})
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
