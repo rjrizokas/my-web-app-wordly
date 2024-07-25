a6d77cf7ff76820ed438cf0bdc06e3dc65349841
rjrizokas <152369340+rjrizokas@users.noreply.github.com>
2024-07-25 15:32:58 +0200

    Update update.js

--- update.js
+++ update.js
@@ -1,1 +1,24 @@
+document.getElementById('updateForm').addEventListener('submit', async (event) => {
+    event.preventDefault(); // Предотвращаем стандартное действие формы
 
+    const wordInput = document.getElementById('wordInput').value;
+    
+    try {
+        const response = await fetch('http://localhost:5000/update_word', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify({ word: wordInput }),
+        });
+
+        if (response.ok) {
+            document.getElementById('responseMessage').innerText = 'Word updated successfully!';
+        } else {
+            document.getElementById('responseMessage').innerText = 'Failed to update word.';
+        }
+    } catch (error) {
+        console.error('Error:', error);
+        document.getElementById('responseMessage').innerText = 'Error updating word.';
+    }
+});
