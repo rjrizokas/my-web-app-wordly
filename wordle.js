ef854601ebce3c587ab348b5c7c28df7b15f11dc
rjrizokas <152369340+rjrizokas@users.noreply.github.com>
2024-07-25 21:14:04 +0200

    Update wordle.js

--- wordle.js
+++ wordle.js
@@ -13,13 +13,8 @@ let guessList = ["ааааа"]
 guessList = guessList.concat(wordList);
 
 window.onload = function() {
-    intialize();
+    initialize();
     fetchWord(); // Fetch the word from the server on page load
-
-    // Add event listener for the Update Word button
-    // document.getElementById('updateWord').addEventListener('click', () => {
-    //     fetchWord(); // Fetch the word again when the button is clicked
-    // });
 }
 
 async function fetchWord() {
@@ -33,7 +28,7 @@ async function fetchWord() {
     }
 }
 
-function intialize() {
+function initialize() {
     // Create the game board
     for (let r = 0; r < height; r++) {
         for (let c = 0; c < width; c++) {
@@ -59,7 +54,6 @@ function intialize() {
 
         for (let j = 0; j < currRow.length; j++) {
             let keyTile = document.createElement("div");
-
             let key = currRow[j];
             keyTile.innerText = key;
             if (key == "Enter") {
@@ -72,11 +66,7 @@ function intialize() {
 
             keyTile.addEventListener("click", processKey);
 
-            if (key == "Enter") {
-                keyTile.classList.add("enter-key-tile");
-            } else {
-                keyTile.classList.add("key-tile");
-            }
+            keyTile.classList.add(key == "Enter" ? "enter-key-tile" : key == "⌫" ? "backspace-key-tile" : "key-tile");
             keyboardRow.appendChild(keyTile);
         }
         document.body.appendChild(keyboardRow);
@@ -98,16 +88,9 @@ function processKey() {
     let key = this.id;
     let letter = this.innerText;
 
-    if (key === "Enter" || key === "Backspace") {
-        let event = { "code": key };
-        processInput(event);
-    } else if (key.startsWith("Key")) {
-        let event = { "code": key };
-        processInput(event);
-    } else {
-        let event = { "code": "Key" + letter };
-        processInput(event);
-    }
+    let event = { "code": key === "Enter" ? "Enter" : key === "Backspace" ? "Backspace" : "Key" + letter };
+
+    processInput(event);
 }
 
 function processInput(e) {
@@ -218,4 +201,5 @@ function update() {
     row += 1;
     col = 0;
 }
+
 
