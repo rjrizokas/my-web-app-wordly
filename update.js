995cc8b7ff75f524243fd9c7413a9dc898d1707d
rjrizokas <152369340+rjrizokas@users.noreply.github.com>
2024-07-25 20:29:35 +0200

    Update update.js

--- update.js
+++ update.js
@@ -1,25 +1,30 @@
 async function updateAllWords() {
     const words = {
-        word1: document.getElementById('word1').value,
-        word2: document.getElementById('word2').value,
-        word3: document.getElementById('word3').value,
-        word4: document.getElementById('word4').value,
-        word5: document.getElementById('word5').value,
-        word6: document.getElementById('word6').value,
-        word7: document.getElementById('word7').value
+        monday: document.getElementById('word1').value,
+        tuesday: document.getElementById('word2').value,
+        wednesday: document.getElementById('word3').value,
+        thursday: document.getElementById('word4').value,
+        friday: document.getElementById('word5').value,
+        saturday: document.getElementById('word6').value,
+        sunday: document.getElementById('word7').value
     };
 
-    const response = await fetch('https://my-web-app-wordly.onrender.com/update_word', {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json'
-        },
-        body: JSON.stringify(words)
-    });
+    try {
+        const response = await fetch('https://my-web-app-wordly.onrender.com/update_word', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(words)
+        });
 
-    if (response.ok) {
-        alert('Слова успешно обновлены!');
-    } else {
-        alert('Ошибка обновления слов.');
+        if (response.ok) {
+            alert('Слова успешно обновлены!');
+        } else {
+            alert('Ошибка обновления слов.');
+        }
+    } catch (error) {
+        console.error('Ошибка:', error);
+        alert('Ошибка при отправке запроса.');
     }
 }
