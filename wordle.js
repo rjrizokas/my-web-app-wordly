43ea2c8448a1495bc1e104167373da91339fe12d
rjrizokas <152369340+rjrizokas@users.noreply.github.com>
2024-07-25 20:48:37 +0200

    Update wordle.js

--- wordle.js
+++ wordle.js
@@ -47,10 +47,11 @@ function intialize() {
 
     // Create the keyboard
     let keyboard = [
-        ["Q", "W", "E", "R", "T", "Y", "U", "I", "O", "P"],
-        ["A", "S", "D", "F", "G", "H", "J", "K", "L", " "],
-        ["Enter", "Z", "X", "C", "V", "B", "N", "M", "⌫"]
+        ["Й", "Ц", "У", "К", "Е", "Н", "Г", "Ш", "Щ", "З", "Х", "Ъ"],
+        ["Ф", "Ы", "В", "А", "П", "Р", "О", "Л", "Д", "Ж", "Э", " "],
+        ["Enter", "Я", "Ч", "С", "М", "И", "Т", "Ь", "Б", "Ю", "⌫"]
     ];
+
 
     for (let i = 0; i < keyboard.length; i++) {
         let currRow = keyboard[i];
