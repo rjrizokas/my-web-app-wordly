7ca54c3bd13fc9c7332c47488d452d7377e541fb
rjrizokas <152369340+rjrizokas@users.noreply.github.com>
2024-07-25 23:59:04 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,34 +1,50 @@
 from flask import Flask, request, jsonify
 from flask_cors import CORS
 import datetime
+import json
+import os
 
 app = Flask(__name__)
 CORS(app)
 
-# Переменные для хранения слов
-words = {
-    "monday": "СТЕНА",
-    "tuesday": "ПОТОП",
-    "wednesday": "КАРТА",
-    "thursday": "ПЛЕЧО",
-    "friday": "ВЕСНА",
-    "saturday": "БОЧКА",
-    "sunday": "МЕСТО"
-}
+# Путь к файлу words.json
+WORDS_FILE = 'words.json'
+
+# Функция для чтения слов из файла
+def read_words():
+    if not os.path.exists(WORDS_FILE):
+        return {
+            "monday": "WORLD",
+            "tuesday": "PLANE",
+            "wednesday": "STAKE",
+            "thursday": "BROWN",
+            "friday": "SWEET",
+            "saturday": "BLIND",
+            "sunday": "STONE"
+        }
+    with open(WORDS_FILE, 'r', encoding='utf-8') as file:
+        return json.load(file)
+
+# Функция для записи слов в файл
+def write_words(words):
+    with open(WORDS_FILE, 'w', encoding='utf-8') as file:
+        json.dump(words, file, ensure_ascii=False, indent=4)
 
 @app.route('/get_word', methods=['GET'])
 def get_word():
-    # Если параметр day_of_week отсутствует, используем текущий день недели
+    words = read_words()
     day_of_week = request.args.get('day_of_week', datetime.datetime.now().strftime('%A').lower())
     word = words.get(day_of_week, "SNAKE")
-    return jsonify({"word": word})  # Возвращаем ответ с корректным кодированием
+    return jsonify({"word": word})
 
 @app.route('/update_word', methods=['POST'])
 def update_word():
     data = request.json
+    words = read_words()
     for key in data:
         if key in words:
             words[key] = data[key]
+    write_words(words)
     return jsonify({"message": "Words updated successfully!"})
 
 if __name__ == '__main__':
