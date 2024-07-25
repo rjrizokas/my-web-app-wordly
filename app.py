f5d2b264254c95b75ff4ed22819bc0211b61c2e2
rjrizokas <152369340+rjrizokas@users.noreply.github.com>
2024-07-25 20:08:03 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,47 +1,34 @@
 from flask import Flask, request, jsonify
 from flask_cors import CORS
-
+import datetime
 
 app = Flask(__name__)
 CORS(app)
 
 # Переменные для хранения слов
 words = {
-    "word1": "СЛОВО",
-    "word2": "КНИГА",
-    "word3": "СРЕДА",
-    "word4": "БУКВА",
-    "word5": "КРЕСТ",
-    "word6": "ВЕСНА",
-    "word7": "ЗВЕНО"
+    "monday": "СЛОВО",
+    "tuesday": "КНИГА",
+    "wednesday": "СРЕДА",
+    "thursday": "БУКВА",
+    "friday": "КРЕСТ",
+    "saturday": "ВЕСНА",
+    "sunday": "ЗВЕНО"
 }
-
-@app.route('/')
-def index():
-    return "Welcome to the Wordly API!"
 
 @app.route('/get_word', methods=['GET'])
 def get_word():
-    day_of_week = request.args.get('day_of_week')
-    if day_of_week:
-        word = words.get(day_of_week, "Not Found")
-        return jsonify({"word": word})
-    else:
-        # Возвращаем все слова, если параметр не указан
-        return jsonify(words)
-
+    # Вернем слово для текущего дня недели
+    day_of_week = datetime.datetime.now().strftime('%A').lower()
+    return jsonify({"word": words.get(day_of_week, "SNAKE")})
 
 @app.route('/update_word', methods=['POST'])
 def update_word():
     data = request.json
-    print("Received data:", data)  # Логирование полученных данных
     for key in data:
         if key in words:
             words[key] = data[key]
     return jsonify({"message": "Words updated successfully!"})
 
-
 if __name__ == '__main__':
-    app.run(debug=True)
-
-
+    app.run(host='0.0.0.0', port=5000)
