827a2e526043dc96ab35b6e9392e4a208aaf1158
rjrizokas <152369340+rjrizokas@users.noreply.github.com>
2024-07-26 19:26:08 +0200

    Update app.py

--- app.py
+++ app.py
@@ -124,4 +124,4 @@ def update_word1():
         return jsonify({"message": "Error updating words!"}), 500
         
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+    app.run(host='0.0.0.0', port=int(os.getenv('PORT', 5000)))  # Запускаем на порту 5000
