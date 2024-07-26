9d09d5db8b5bed02d22d12a583221bf6e51e871c
rjrizokas <152369340+rjrizokas@users.noreply.github.com>
2024-07-26 19:26:48 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,22 +1,34 @@
-from aiogram import Bot, Dispatcher, types
-from aiogram.utils import executor
+from aiogram import Bot, Dispatcher, executor, types
+from aiogram.types.web_app_info import WebAppInfo
 import os
 
-API_TOKEN = 'YOUR_API_TOKEN'
+API_TOKEN = '7439794203:AAEQGaP_uSsTh7c5onzP1VMrLo9VO1rmmtk'
 
 bot = Bot(token=API_TOKEN)
 dp = Dispatcher(bot)
 
+allowed_users1 = ["vio_goncharova", "nft337"]
+allowed_users2 = ["rjrizo", "nft337"]
+
 @dp.message_handler(commands=['start'])
 async def start(message: types.Message):
-    # Ваш код обработки команды /start...
+    markup = types.ReplyKeyboardMarkup()
 
-async def on_startup(dp):
-    print('Starting bot...')
+    markup.add(types.KeyboardButton('слово от RJ', web_app=WebAppInfo(
+        url='https://rjrizokas.github.io/my-web-app-wordly/wordle.html')))
 
-async def on_shutdown(dp):
-    print('Shutting down bot...')
+    markup.add(types.KeyboardButton('слово от Ви', web_app=WebAppInfo(
+        url='https://rjrizokas.github.io/my-web-app-wordly/wordle1.html')))
+
+    if message.from_user.username in allowed_users2:
+        markup.add(types.KeyboardButton('загадать слово от RJ', web_app=WebAppInfo(
+            url='https://rjrizokas.github.io/my-web-app-wordly/update.html')))
+
+    if message.from_user.username in allowed_users1:
+        markup.add(types.KeyboardButton('загадать слово от Ви', web_app=WebAppInfo(
+            url='https://rjrizokas.github.io/my-web-app-wordly/update1.html')))
+
+    await message.answer('Что наша жизнь?', reply_markup=markup)
 
 if __name__ == '__main__':
-    # Запускаем бота в режиме опроса
-    executor.start_polling(dp, on_startup=on_startup, on_shutdown=on_shutdown)
+    executor.start_polling(dp)
