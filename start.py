a02ae582b1a6a94e6e93b5221b24f14e88dab9b2
rjrizokas <152369340+rjrizokas@users.noreply.github.com>
2024-07-26 19:16:36 +0200

    Update start.py

--- start.py
+++ start.py
@@ -1,7 +1,10 @@
 import subprocess
-import os
+import sys
 
-# Запускаем Flask-приложение с помощью gunicorn
+# Проверяем, что все зависимости установлены
+subprocess.check_call([sys.executable, "-m", "pip", "install", "-r", "requirements.txt"])
+
+# Запускаем Flask-приложение
 subprocess.Popen(["gunicorn", "app:app", "--bind", "0.0.0.0:5000"])
 
 # Запускаем Telegram-бота
