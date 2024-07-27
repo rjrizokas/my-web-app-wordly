a336fd2b105383fd6369797df9f7e83beefef6c4
rjrizokas <152369340+rjrizokas@users.noreply.github.com>
2024-07-28 01:16:21 +0200

    Update update1.js

--- update1.js
+++ update1.js
@@ -1,18 +1,46 @@
-let wordList = [];
+let wordList1 = [];
+let initialWords1 = {};
 
-async function fetchWordList() {
+async function fetchWordList1() {
     try {
         const response = await fetch('https://my-web-app-wordly.onrender.com/get_wordlist');
         const data = await response.json();
-        wordList = data.wordlist.map(word => word.toUpperCase()); // Преобразование слов в верхний регистр для соответствия вводу
-        console.log('Wordlist loaded:', wordList);
+        wordList1 = data.wordlist.map(word => word.toUpperCase());
+        console.log('Wordlist loaded:', wordList1);
     } catch (error) {
         console.error('Ошибка загрузки списка допустимых слов:', error);
         alert('Ошибка загрузки списка допустимых слов.');
     }
 }
 
-async function updateAllWords() {
+async function fetchInitialWords1() {
+    try {
+        const response = await fetch('https://my-web-app-wordly.onrender.com/get_initial_words1');
+        const data = await response.json();
+        if (data.words1) {
+            initialWords1 = data.words1;
+            console.log('Initial words loaded:', initialWords1);
+            displayInitialWords1();
+        } else {
+            console.error('Ошибка загрузки начальных слов:', data.error);
+            alert('Ошибка загрузки начальных слов.');
+        }
+    } catch (error) {
+        console.error('Ошибка загрузки начальных слов:', error);
+        alert('Ошибка загрузки начальных слов.');
+    }
+}
+
+function displayInitialWords1() {
+    const days = ['monday', 'tuesday', 'wednesday', 'thursday', 'friday', 'saturday', 'sunday'];
+    days.forEach((day, index) => {
+        const input = document.getElementById(`word${index + 1}`);
+        input.value = initialWords1[day] || '';
+        input.style.color = 'rgba(128, 128, 128, 0.5)';  // Полупрозрачный серый цвет
+    });
+}
+
+async function updateAllWords1() {
     const words = {
         monday: document.getElementById('word1').value.toUpperCase(),
         tuesday: document.getElementById('word2').value.toUpperCase(),
@@ -30,7 +58,7 @@ async function updateAllWords() {
     }
 
     for (const word of Object.values(words)) {
-        if (!wordList.includes(word)) {
+        if (!wordList1.includes(word)) {
             alert(`Ошибка: слово '${word}' не входит в список допустимых слов.`);
             return;
         }
@@ -57,5 +85,6 @@ async function updateAllWords() {
 }
 
 window.onload = function() {
-    fetchWordList();
+    fetchWordList1();
+    fetchInitialWords1();
 }
