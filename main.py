e1e83f20ff15e7b3d030f1dd449b131c7373b08a
rjrizokas <152369340+rjrizokas@users.noreply.github.com>
2024-07-27 15:34:37 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,18 +1,13 @@
-from aiogram import Bot, Dispatcher, types
-from aiogram.utils.executor import start_webhook
-import os
+from aiogram import Bot, Dispatcher, executor, types
+from aiogram.types.web_app_info import WebAppInfo
 
-API_TOKEN = '7439794203:AAEQGaP_uSsTh7c5onzP1VMrLo9VO1rmmtk'
-
-WEBHOOK_HOST = 'https://my-web-app-wordly-tg.onrender.com'
-WEBHOOK_PATH = '/webhook'
-WEBHOOK_URL = f"{WEBHOOK_HOST}{WEBHOOK_PATH}"
-
-bot = Bot(token=API_TOKEN)
+bot = Bot('7439794203:AAEQGaP_uSsTh7c5onzP1VMrLo9VO1rmmtk')
 dp = Dispatcher(bot)
 
 allowed_users1 = ["vio_goncharova", "nft337"]
 allowed_users2 = ["rjrizo", "nft337"]
+
+
 
 @dp.message_handler(commands=['start'])
 async def start(message: types.Message):
@@ -30,20 +25,4 @@ async def start(message: types.Message):
             url=f'https://rjrizokas.github.io/my-web-app-wordly/update1.html?user_id={user_id}')))
     await message.answer('Что наша жизнь?', reply_markup=markup)
 
-async def on_startup(dp):
-    await bot.set_webhook(WEBHOOK_URL)
-
-async def on_shutdown(dp):
-    await bot.delete_webhook()
-
-if __name__ == '__main__':
-    port = int(os.environ.get('PORT', 5000))
-    start_webhook(
-        dispatcher=dp,
-        webhook_path=WEBHOOK_PATH,
-        on_startup=on_startup,
-        on_shutdown=on_shutdown,
-        skip_updates=True,
-        host='0.0.0.0',
-        port=port
-    )
+executor.start_polling(dp)
