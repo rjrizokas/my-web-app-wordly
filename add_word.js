f52ff2a619575e514d346a602ad721e18285819e
rjrizokas <152369340+rjrizokas@users.noreply.github.com>
2024-07-28 19:48:34 +0200

    Create add_word.js

---
+++ add_word.js
@@ -0,0 +1,32 @@
+document.getElementById('add-word-form').addEventListener('submit', async function(event) {
+    event.preventDefault();
+
+    const newWord = document.getElementById('new-word').value.toUpperCase();
+    const messageDiv = document.getElementById('message');
+
+    if (newWord.length !== 5) {
+        messageDiv.textContent = 'Ошибка: слово должно состоять из 5 букв.';
+        return;
+    }
+
+    try {
+        const response = await fetch('https://my-web-app-wordly.onrender.com/add_word', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({ word: newWord })
+        });
+
+        if (response.ok) {
+            messageDiv.textContent = 'Слово успешно добавлено!';
+            messageDiv.style.color = 'green';
+        } else {
+            const errorData = await response.json();
+            messageDiv.textContent = `Ошибка: ${errorData.message}`;
+        }
+    } catch (error) {
+        console.error('Ошибка:', error);
+        messageDiv.textContent = 'Ошибка при добавлении слова.';
+    }
+});
